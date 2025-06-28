16b8910480f379c2f5b7acdc473d82606ce8b8a9
gareth78 <gareth78@hotmail.com>
2025-06-29 00:48:28 +0100

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,15 @@
 const Home = () => {
+  console.log('Rendering Home component');
   return (
-    <div style={{ textAlign: 'center', marginTop: '5rem', fontSize: '2rem' }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontSize: '2rem',
+      }}
+    >
       TRT Planner
     </div>
   );
